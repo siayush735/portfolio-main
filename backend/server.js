0d7eb0e648be4f941d6c4e0364bf7513af70d274
Ayush <ayushsinha4343@gmail.com>
2026-05-17 21:25:40 +0530

    env file

--- backend/server.js
+++ backend/server.js
@@ -31,3 +31,9 @@ app.post('/api/contact', async (req,res)=>{
     console.log("Error ",err);
   }
 })
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
